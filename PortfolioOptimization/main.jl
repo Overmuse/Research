d0fd579eeb541a0954af741bc8b5e896b1f69228
Sebastian Rollen <rollenseb@gmail.com>
2020-11-16 09:25:05 +0000

    start adding PortfolioOptimization script

---
+++ PortfolioOptimization/main.jl
@@ -0,0 +1,8 @@
+using DataFrames, LibPQ, LinearAlgebra, Statistics
+
+conn = Connection("host=$(ENV["AIAIADB_HOST"]) user=postgres password=$(ENV["AIAIADB_PASSWORD"]) dbname=aiaiadb")
+query = "SELECT * FROM adjusted_prices WHERE datetime >= '2016-06-04'"
+res = DataFrame(execute(conn, query))
+res = transform(groupby(res, :ticker), :close => (x -> vcat(missing, x[2:end] ./ x[1:end-1])) => :return)
+returns = unstack(res[:, [:datetime, :ticker, :return]], :ticker, :return)
+cov(Matrix(returns[2:end, 2:end]))
